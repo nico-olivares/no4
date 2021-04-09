5d2bc3bb653a01b46aaf62f755dfa95dbd451508
Nicolas Olivares <nolivaresr@hotmail.com>
2021-04-09 17:45:51 -0400

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='theHeader'>this is the header</header>
+      <main className='theMain'>
+        <div className="left-panel">
+            left panel
+        </div>
+        <div className="center-panel">center panel</div>
+        <div className="right-panel">right panel</div>
+      </main>
+      <footer className="theFooter">this is the footer</footer>
     </div>
   );
 }
