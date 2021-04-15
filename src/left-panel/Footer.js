9feb498a081957e63b11c5535cb0af6aa4865155
Nicolas Olivares <nolivaresr@hotmail.com>
2021-04-15 17:07:51 -0400

    perfecting left panel

--- src/left-panel/Footer.js
+++ src/left-panel/Footer.js
@@ -1,10 +1,19 @@
 import './Footer.css';
+import { Icon } from 'semantic-ui-react'
 
 
 function Footer() {
   return (
     <div className="footer-inner">
-        Footer
+        <a href='https://github.com/nico-olivares' target='_blank'>
+          <Icon name="github" size='big' color='black' />
+        </a>
+        <a href='mailto:nolivaresr@hotmail.com'>
+          <Icon name='envelope' size='big' color='black' />
+        </a>
+        <a href='https://www.linkedin.com/in/nicolas-olivares-developer/' target="_blank">
+          <Icon name='linkedin' size='big' color='blue' />
+        </a>
     </div>
   );
 }
