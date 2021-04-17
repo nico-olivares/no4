0631d5e0bebf1fdc46c758de2a5edd2e05932b5b
Nicolas Olivares <nolivaresr@hotmail.com>
2021-04-16 21:27:16 -0400

    working on left panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 import LeftPanel from './left-panel/LeftPanel';
 
 function App() {
+
+  const [expandedMenu, setExpandedMenu ] = useState(true);
+
   return (
     <div className="App">
-        <div className="left-panel">
+        <div className="left-panel left-panel-reduced" style={expandedMenu ? '{{width: '20vw' }}' : '{{ width: '3vw'}}'  } >
             <LeftPanel />
         </div>
         <div className="center-panel">center panel</div>
--- src/left-panel/LeftPanel.js
+++ src/left-panel/LeftPanel.js
@@ -2,13 +2,19 @@ import './LeftPanel.css';
 import Header from './Header.js';
 import LeftMenu from './LeftMenu.js';
 import Footer from './Footer.js';
+import { Icon } from 'semantic-ui-react';
 
 function LeftPanel() {
   return (
     <div className="left-panel-inner">
-        <Header  />
-        <LeftMenu  />
-        <Footer  />
+        <div className="left-panel-content hide"  >
+          <Header  />
+          <LeftMenu  />
+          <Footer  />
+        </div>
+        <div className="left-panel-arrow" >
+          <Icon name="angle double left" size='large' color='black' />
+        </div>
     </div>
   );
 }
