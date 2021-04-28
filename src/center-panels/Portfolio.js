92d022a4865160385ccdf5552898b3fac867a95b
Nicolas Olivares <nolivaresr@hotmail.com>
2021-04-28 15:26:08 -0400

    small progress with the portfolio part

--- src/center-panels/Portfolio.js
+++ src/center-panels/Portfolio.js
@@ -1,41 +1,75 @@
-import { Grid, Embed } from 'semantic-ui-react';
+import './Portfolio.css'
+import { Grid, Embed, Modal, Button, Image, Header } from 'semantic-ui-react';
+import { useState } from 'react'
 
 function Portfolio({item}) {
+    const [ open, setOpen ] = useState(false);
 
 return (
     <div className="portfolio-container">
-        <Grid.Row>
-            <h1>Title: {item.title}</h1>
-        </Grid.Row>
-        <Grid.Row>
-            <p>Readme: {item.description}</p>
-        </Grid.Row>
-        <Grid.Row>
-            <Grid.Column>
-                <p>Stack used:</p>
-            </Grid.Column>
-            {item.stack.map((stackItem) => <Grid.Column><p>{stackItem}</p></Grid.Column>)}
-        </Grid.Row>
-        <Grid.Row>
-            <Grid.Column>
-                <p>Github:</p>
-            </Grid.Column>
-            <Grid.Column>
-                <p>{item.github}</p>
-            </Grid.Column>
-            
-        </Grid.Row>
-        <Grid.Row>
-            <Grid.Column>
-                <p>Deployment:</p>
-            </Grid.Column>
-            <Grid.Column>
-                <p>{item.deployment}</p>
-            </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-            <Embed className='embedFrame' icon='right circle arrow' placeholder="" url={item.deployment} />
-        </Grid.Row>
+        <Grid divided >
+            <Grid.Row>
+                <Grid.Column>
+                    <h1>Title: {item.title}</h1>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column>
+                    <p>Readme: {item.description}</p>
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={4} >
+                <Grid.Column>
+                    <p>Stack:</p>
+                </Grid.Column>
+                {item.stack.map((stackItem) => <Grid.Column><p>{stackItem}</p></Grid.Column>)}
+            </Grid.Row>
+            <Grid.Row>
+                    <Grid.Column>
+                        <p>Github: <a href={item.github} >{item.github}</a></p>
+                    </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                    <Grid.Column>
+                        <p>Deployment: <a href={item.deployment} >{item.deployment}</a></p>
+                    </Grid.Column>
+            </Grid.Row>
+            <Grid.Row centered columns={1} >
+                <Grid.Column className='image-column'>
+                        <Modal closeIcon size='fullscreen'
+                        onClose={() => setOpen(false)}
+                        onOpen={() => setOpen(true)}
+                        open={open}
+                        trigger={<a href='#' ><Image src={item.image} size='large' /></a>}
+                        >
+                        <Modal.Header>Select a Photo</Modal.Header>
+                        <Modal.Content image>
+                            <Image size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' wrapped />
+                            <Modal.Description>
+                            <Header>Default Profile Image</Header>
+                            <p>
+                                We've found the following gravatar image associated with your e-mail
+                                address.
+                            </p>
+                            <p>Is it okay to use this photo?</p>
+                            </Modal.Description>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button color='black' onClick={() => setOpen(false)}>
+                            Nope
+                            </Button>
+                            <Button
+                            content="Yep, that's me"
+                            labelPosition='right'
+                            icon='checkmark'
+                            onClick={() => setOpen(false)}
+                            positive
+                            />
+                        </Modal.Actions>
+                    </Modal>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid  >
     </div>
 )
 }
--- src/center-panels/portfolioItems.js
+++ src/center-panels/portfolioItems.js
@@ -1,13 +1,14 @@
-function portfolioItemObject(title, description, stack, github, deployment) {
+function portfolioItemObject(title, description, stack, github, deployment, imageUrl) {
     this.title = title;
     this.description = description;
     this.stack = stack;
     this.github = github;
     this.deployment = deployment;
+    this.image = './media/apps/' + imageUrl;
 }
 
 const portfolioItems = [
-    new portfolioItemObject('Doomsday Preppers', 'A website about prepping', ['js', 'React'], 'https://github.com/nico-olivares/pandemicprep2', 'https://panprep2.herokuapp.com/')
+    new portfolioItemObject('Doomsday Preppers', 'A website about prepping', ['js', 'React', 'node', 'Express', 'webtoken', 'npm'], 'https://github.com/nico-olivares/pandemicprep2', 'https://panprep2.herokuapp.com/', 'doomsday.png')
 ]
 
 function getPortfolioItem(name) {
