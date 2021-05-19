42bb32ef1989cb625286a3d2e15a89215763d447
Nicolas Olivares <nolivaresr@hotmail.com>
2021-05-19 13:14:03 -0400

    trying to add carousel to references

--- src/center-panels/Contact.js
+++ src/center-panels/Contact.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Contact.css';
+import { Transition } from 'semantic-ui-react';
 
 //contact component
-export default function Contact() {
+export default function Contact({ panelIsVisible }) {
 
 
     return (
+        <Transition visible={panelIsVisible} animation='fly left' duration={1500}>
         <div>These are my Contacts</div>
+        </Transition>
     )
 
 }
--- src/center-panels/References.js
+++ src/center-panels/References.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './References.css';
+import { Transition } from 'semantic-ui-react';
+import { Carousel } from './Carousel.js';
 
-export default function References() {
+export default function References({ panelIsVisible }) {
 
 
     return (
-        <div>These are my References</div>
+        <Transition visible={panelIsVisible} animation='fly left' duration={1500}>
+            <Carousel />
+        </Transition >
     )
 
 }
