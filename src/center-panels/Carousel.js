c64d5d1bd114cbb9eb278d9e3af01bd2bb93fbb2
Nicolas Olivares <nolivaresr@hotmail.com>
2021-05-20 17:39:51 -0400

    working on the carousel, especifically the timeout part

--- src/center-panels/Carousel.js
+++ src/center-panels/Carousel.js
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { act } from 'react-dom/test-utils';
 import { Transition, Card, Button } from 'semantic-ui-react';
+import './Carousel.css'
 
 
 export default function Carousel({ references, interval }) {
 const back = '<<';
 const forward = '>>';
-console.log('the references are ', references);
+
+const myUseEffectTimeout = () => {
+    setTimeout(() => {
+        if (activeIndex === references.length -1) {
+            setActiveIndex(0);
+        } else {
+            setActiveIndex(activeIndex + 1);
+        }
+    }, interval
+)
+}
+
+const [ activeIndex, setActiveIndex ] = useState(0);
+
+useEffect(() => {
+    clearTimeout(myUseEffectTimeout);
+    myUseEffectTimeout();
+}, [activeIndex]);
+
     return (
-        <div>
-            <Button primary>
+        <div className='carousel' >
+            <Button primary onClick={() => {
+                if (activeIndex === 0) {
+                    setActiveIndex(references.length - 1);
+                } else {
+                    setActiveIndex(activeIndex - 1);
+                }
+                clearTimeout(myUseEffectTimeout);
+            }} >
                 {back}
             </Button>
             {references.map((item, index) => {
                 return (
-                    <Card index={index} >
-                        <Card.Content header={item.from} />
-                        <Card.Content description={item.reference} />
-                    </Card>
+                    <Transition visible={index === activeIndex ? true : false } animation={index === activeIndex ? 'fade left' : 'fade right'} duration={600} >
+                        <Card index={index} >
+                            <Card.Content header={item.from} />
+                            <Card.Content description={item.reference} />
+                        </Card>
+                    </Transition>
                 )
             })}
-            <Button primary>
+            <Button primary onClick={() => {
+                if (activeIndex === references.length - 1) {
+                    setActiveIndex(0);
+                } else {
+                    setActiveIndex(activeIndex + 1);
+                }
+                clearTimeout(myUseEffectTimeout);
+            }} >
                 {forward}
             </Button>
 
