58a3533b0050463db1f8d091e8094b813d04002f
Nicolas Olivares <nolivaresr@hotmail.com>
2021-06-03 12:55:27 -0400

    experience and education are completed. Working on skills now

--- src/center-panels/Skills.js
+++ src/center-panels/Skills.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Skills.css';
+import getSkillsItem from '../data/skillsItems';
+// import ExperienceCard from './ExperienceCard';
+import { Transition, Container, Card, Label } from 'semantic-ui-react';
 
-export default function Skills() {
+//experince component.
+export default function Skills({item, panelIsVisible}) {
 
+    console.log(item);
 
     return (
-        <div>These are my Skills</div>
+        <div>
+            <Transition visible={panelIsVisible} animation='fly left' duration={1500}  >
+                <div className='skills-container' >
+                    <h2> Skills</h2>
+                    <div className='skill-subcontainer' >
+                        <Container className='skill-cards-container' fluid color='red' >
+                            <p>Hello world</p>
+                        </Container>
+                    </div>
+                    
+                </div>
+            </Transition>
+        </div>
+        
     )
 
 }
--- src/data/experienceItems.js
+++ src/data/experienceItems.js
@@ -5,26 +5,29 @@ const experienceItems = [
         title: 'Programming', 
         children: [
             {
+                title: 'JavaScript Full Stack',
+                years: '2020 to present',
+                institution: 'Fullstack Academy student and self',
+                description: `From April to October of 2020 I attended a Fullstack bootcamp by UNF - Fullstack Academy. There I learned the JavaScript full stack including HTML, CSS, JavaScript, Express, PostgreSQL, React, node, git, github, and more. The camp was project driven. Each week (sometimes longer periods) we would code a project. The last two projects were group efforts and involved git control and team work. I got to be the leader of the second group for the Capstone project, the largest project of the camp. After the camp was over I have kept coding and learning in order to keep my skills sharp and improve as much as I can.`
+            },
+            {
+                title: 'Java',
+                years: '2010-2018',
+                institution: 'self',
+                description: `When my school moved back to PCs I started learning Java in an effort to be able to keep developing for my classroom. Java was perfect for my needs as it was multi-platform and we still had some macs around. At first I rewrote some of the same apps I had first coded in Objective-C, but later I wrote some new apps as well.`
+             }, 
+             {
+                title: 'Objective-C',
+                years: '2006-2012',
+                institution: 'self',
+                description: `When my school got mac computers for teachers and students I taught myself Objective-C and coded a few apps for my classroom using Xcode. Some of the apps I later rewrote in Java when my school moved to PCs.`
+            }, 
+            {
                 title: 'Pascal',
                 years: 'circa 1994',
                 institution: 'Universidad Adolfo Ibañez - Student',
                 description: `During my business administration studies I took a CS class based on Pascal. As part of the course we developed a basic bank application using Pascal.`
-             }, {
-                 title: 'Objective-C',
-                 years: '2006-2012',
-                 institution: 'self',
-                 description: `When my school got mac computers for teachers and students I taught myself Objective-C and coded a few apps for my classroom using Xcode. Some of the apps I later rewrote in Java when my school moved to PCs.`
-             }, {
-                 title: 'Java',
-                 years: '2010-2018',
-                 institution: 'self',
-                 description: `When my school moved back to PCs I started learning Java in an effort to be able to keep developing for my classroom. Java was perfect for my needs as it was multi-platform and we still had some macs around. At first I rewrote some of the same apps I had first coded in Objective-C, but later I wrote some new apps as well.`
-              }, {
-                  title: 'JavaScript Full Stack',
-                  years: '2020 to present',
-                  institution: 'Fullstack Academy student and self',
-                  description: `From April to October of 2020 I attended a Fullstack bootcamp by UNF - Fullstack Academy. There I learned the JavaScript full stack including HTML, CSS, JavaScript, Express, PostgreSQL, React, node, git, github, and more. The camp was project driven. Each week (sometimes longer periods) we would code a project. The last two projects were group efforts and involved git control and team work. I got to be the leader of the second group for the Capstone project, the largest project of the camp. After the camp was over I have kept coding and learning to keep my skills sharp and improve as much as I can.`
-              }
+             } 
         ]
     }, {
         title: 'Educating',
