3755aa8d0a7bb7ce3995959d4cfb100f2793bd0e
Nicolas Olivares <nolivaresr@hotmail.com>
2021-07-09 14:17:57 -0400

    got rid of note

--- src/center-panels/About.js
+++ src/center-panels/About.js
@@ -23,12 +23,7 @@ function About({ panelIsVisible }) {
                             <li>The photos in the landing page are of places that are dear to me. They have all been taken by me personally.</li>
                         </ul>
                         <br/><br />
-                        <p className="note" >
-                        <strong>Note: </strong>
-                        The project is not fully complete, but it is already better than the page I used to have so I published it. For now, the carousel 
-                        component has a small glitch and the whole app needs to be optimized for tablets and phones.<br />
-                        I will keep working and making it better.
-                        </p>
+                        
                         
                     </div>
                 </Transition >
