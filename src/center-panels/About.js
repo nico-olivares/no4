71843daa1bbda87dc5edd40f963eb77e0b56742b
Nicolas Olivares <nolivaresr@hotmail.com>
2021-07-09 20:35:25 -0400

    small addition to the about page

--- src/center-panels/About.js
+++ src/center-panels/About.js
@@ -10,8 +10,8 @@ function About({ panelIsVisible }) {
             <div className="about-container">
                 <Transition visible={panelIsVisible} animation='fly left' duration={1500}>
                     <div className='large-text' >
-                        <p>This webpage was designed and implemented by <strong>Nicolas Olivares</strong> using the following:</p> <br/>
-                        
+                        <h2>About this webpage...</h2><br />
+                        <p>This webpage was designed and implemented by <strong>Nicolas Olivares</strong> using the following:</p>
                         <ul>
                             <li>React and the Semantic UI framework.</li>
                             <li>With React: JavaScript, HTML, CSS.</li>
@@ -19,11 +19,13 @@ function About({ panelIsVisible }) {
                         <br/>
                         <p>Please explore and enjoy!</p>
                         <br/>
-                        <ul>
-                            <li>The photos in the landing page are of places that are dear to me. They have all been taken by me personally.</li>
-                        </ul>
+                        <p>
+                            <strong className='note' >Note: </strong>The photos in the landing page are of places that are dear to me. 
+                            They have all been taken by me personally.
+                        </p>
                         <br/><br />
-                        
+                        <h2>About my name...</h2><br />
+                        <p>In hispanic countries we have two surnames. My legal name is <strong>Nicolas Olivares Reitze</strong> where Olivares is not my middle name (I don't have one), but instead Olivares is the first half of my surname. I don't use the second half of my surname except for legal matters. Most hispanic people do the same, only use the first half of their surnames.</p>
                         
                     </div>
                 </Transition >
